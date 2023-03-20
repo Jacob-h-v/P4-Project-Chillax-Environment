c74474a49d6c383b1ec27a18deb9993f4138dcd9
Jacob-h-v <jvillu21@student.aau.dk>
2023-03-20 11:36:48 +0100

    added rotations to capsulius

--- Assets/Scripts/ManualInput.cs
+++ Assets/Scripts/ManualInput.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class ManualInput : MonoBehaviour
 {
+    private float moveAD;
+    private float moveWS;
+    //private float gravity = -20f;
+    //private float jumpHeight = 1f;
+    public float speed = 1f;
+    //private Vector3 velocity;
+    //public CharacterController charC;
+    public Transform playerObject;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +21,22 @@ public class ManualInput : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        moveAD = Input.GetAxis("Horizontal");
+        moveWS = Input.GetAxis("Vertical");
+
+        //Vector3 movePlayer = transform.right * moveAD + transform.forward * moveWS;
+
+        //charC.Move(movePlayer * speed * Time.deltaTime);
+
+        //{
+         //   velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        //}
+
+        //velocity.y += gravity * Time.deltaTime;
+        //charC.Move(velocity * Time.deltaTime);
+
+        playerObject.Rotate(Vector3.up * speed * moveAD);
+        playerObject.Rotate(Vector3.right * speed * moveWS);
     }
 }
