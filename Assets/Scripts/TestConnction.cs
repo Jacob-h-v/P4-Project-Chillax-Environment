a96e649ba533c6cdc56c382b587c5470790f4610
Jacob-h-v <jvillu21@student.aau.dk>
2023-03-20 15:31:47 +0100

    TestConnection updated

--- Assets/Scripts/TestConnction.cs
+++ Assets/Scripts/TestConnction.cs
@@ -1,28 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Threading;
 using System.IO.Ports;
 
 public class TestConnction : MonoBehaviour
-{
-    SerialPort data_stream = new SerialPort("\\\\.\\COM3",19200); // Arduino is connected to COM3, with 19200 baud rate
-    public string receivestring;
+{ 
+    Thread IOThread = new Thread(DataThread);
+    private static SerialPort data_stream;
+    private static string incomingMsg = "";
+    private static string outgoingMsg = "";
+    /*public string receivestring;
     public GameObject test_data;
     public Rigidbody rb;
     public float sensitivity = 0.01f;
-
     public string[] datas;
+    */
 
+    private static void DataThread()
+    {
+        data_stream = new SerialPort("COM3", 19200);
+        data_stream.Open();
+
+        while(true)
+        {
+            if (outgoingMsg != "")
+            {
+                data_stream.Write(outgoingMsg);
+                outgoingMsg = "";
+            }
+            incomingMsg = data_stream.ReadExisting();
+            Thread.Sleep(200);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        IOThread.Abort();
+        data_stream.Close();
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        data_stream.Open(); //Initiate the Serial stream
+        IOThread.Start();
+        //data_stream.Open(); //Initiate the Serial stream
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (incomingMsg != "")
+        {
+            Debug.Log(incomingMsg);
+        }
+        /*
+        if (!data_stream.IsOpen){
+        data_stream = new SerialPort("COM3", 19200);
+        data_stream.ReadTimeout = 1000;
+        data_stream.WriteTimeout = 1000;
+        data_stream.Open();
+        }
         receivestring = data_stream.ReadLine();
 
         string[] datas = receivestring.Split(','); // split the data between ','
@@ -31,5 +69,6 @@ public class TestConnction : MonoBehaviour
         transform.Rotate(0, float.Parse(datas[2]), 0);
 
         Debug.Log(receivestring);
+        */
     }
 }
