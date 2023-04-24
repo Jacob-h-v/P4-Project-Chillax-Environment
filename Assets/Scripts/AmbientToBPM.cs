bb9c42a09c901459b05466b049644a263abc05fa
RasmusKanne <rasmuskanne@gmail.com>
2023-04-24 13:08:35 +0200

    Comments + Sound Volume = BPM
    Sound volume of amient music set to BPM / 200

--- Assets/Scripts/AmbientToBPM.cs
+++ Assets/Scripts/AmbientToBPM.cs
@@ -21,10 +21,13 @@ public class AmbientToBPM : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // The BPM float from the user object is acessed each update cycle and syncronized with the internal BPM float
         InternalBPM = playerBPM.GetComponent<HeartBeatEmulator>().BPM;
 
-        Debug.Log(InternalBPM);
+        // Testing that the BPM is accurately sent
+        // Debug.Log(InternalBPM);
 
+        // By using the lipPD library we send the BPM float to the pd patch that sends the float as a message to change the generated musics BPM
         pdPatch.SendFloat("BPM", InternalBPM);
     }
 }
